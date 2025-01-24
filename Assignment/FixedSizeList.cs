dfb110d3045d73df8fd162dc8c878ab1236131a0
malakelsayyad <malakelsayyad@yahoo.com>
2025-01-24 20:21:18 +0200

    assignment

---
+++ Assignment/FixedSizeList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class FixedSizeList<T>
+    {
+        public T[] Elements { get; set; }
+        public int Capacity { get; set; }
+        public int Count { get; set; }
+        public FixedSizeList(int capacity) 
+        { 
+           Capacity = capacity;
+           Elements = new T[capacity];
+           Count = 0;
+        }
+
+        public void Add(T item) 
+        {
+            if (Count >= Capacity)
+               throw new Exception("List is full");
+            else
+                Elements[Count] = item;
+                Count++;
+        }
+
+        public T Get(int index)
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new Exception("Index is out of range.");
+            }
+            return Elements[index];
+        }
+    }
+}
---
+++ Assignment/Range.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Range<T> where T: IComparable<T>, INumber<T>
+    {
+        public T Max { get; set; }
+        public T Min { get; set; }
+
+        public Range(T min, T max)
+        {
+            if (min.CompareTo(max) > 0)
+            {
+               Methods.SWAP(ref max,ref min);
+            }
+            Max = max;
+            Min = min;
+
+        }
+
+        public bool isInRange(T value)
+        {
+            return value.CompareTo(Min) >=0 && value.CompareTo(Max)<=0;
+        }
+
+        public T Length()
+        {
+            return Max - Min;
+        }
+
+        public int CompareTo(Range<T>? other)
+        {
+            if (other is not null)
+            {
+                int maxComparison = Max.CompareTo(other.Max);
+                if (maxComparison != 0)
+                {
+                    return maxComparison; 
+                }
+
+                return Min.CompareTo(other.Min);
+            }
+            return 1;
+        }
+    }
+}
